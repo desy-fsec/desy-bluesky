8549407448ac75cbc94b1df62ee314a4b6c4fe29
Devin Burke <devin.burke@desy.de>
2024-07-09 11:59:52 +0200

    Fixed pathing

--- desy_bluesky/devices/__init__.py
+++ desy_bluesky/devices/__init__.py
@@ -1,12 +1,12 @@
-from dgg2 import DGG2Timer
-from gated_counter import GatedCounter
-from mca8715 import MCA8715
-from omsvme58 import OmsVME58Motor
-from sis3820 import SIS3820Counter
-from undulator import Undulator
-from vc_counter import VcCounter
-from vm_motor import VmMotor
-from pilc import PiLC
+from .dgg2 import DGG2Timer
+from .gated_counter import GatedCounter
+from .mca8715 import MCA8715
+from .omsvme58 import OmsVME58Motor
+from .sis3820 import SIS3820Counter
+from .undulator import Undulator
+from .vc_counter import VcCounter
+from .vm_motor import VmMotor
+from .pilc import PiLC
 
 __all__ = [
     "DGG2Timer",
