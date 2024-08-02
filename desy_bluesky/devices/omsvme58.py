5f317b757fdc524ac1dc6e9bfadc28004af13277
Devin Burke <devin.burke@desy.de>
2024-08-02 12:34:43 +0200

    Cleaned up set and added method to retrieve dataclass of config variables.

--- desy_bluesky/devices/omsvme58.py
+++ desy_bluesky/devices/omsvme58.py
@@ -2,23 +2,27 @@ from __future__ import annotations
 
 import asyncio
 from asyncio import Event
-from typing import Optional
+import time
+from typing import Optional, List, Callable
+from dataclasses import dataclass
 
-from bluesky.protocols import Movable, Stoppable
+from bluesky.protocols import Movable, Stoppable, Preparable
+
+from ophyd_async.core.signal import observe_value
 
 from ophyd_async.core import (
+    WatchableAsyncStatus,
     AsyncStatus,
     ConfigSignal,
     HintedSignal,
-    SignalR,
-    WatchableAsyncStatus,
+    SignalRW,
+    SignalX
 )
-from ophyd_async.core.signal import observe_value
 from ophyd_async.core.utils import (
     DEFAULT_TIMEOUT,
     CalculatableTimeout,
     CalculateTimeout,
-    WatcherUpdate,
+    WatcherUpdate
 )
 from ophyd_async.tango import (
     TangoReadableDevice,
@@ -28,9 +32,61 @@ from ophyd_async.tango import (
 )
 from tango import DevState
 
+@dataclass
+class OmsVME58MotorConfig:
+    baserate: Optional[int] = None
+    slewrate: Optional[int] = None
+    conversion: Optional[float] = None
+    acceleration: Optional[int] = None
 
 # --------------------------------------------------------------------
-class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
+class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable, Preparable):
+    """
+    0ms VME58 motor device class
+    
+    Parameters
+    ----------
+    trl : str
+        The Tango device server address
+    name : str
+        The name of the motor
+    sources : dict
+        A dictionary of the sources for the motor signals
+    
+    Attributes
+    ----------
+    position : SignalRW[float]
+        The current position of the motor
+    baserate : SignalRW[int]
+        The base rate of the motor
+    slewrate : SignalRW[int]
+        The slew rate of the motor
+    conversion : SignalRW[float]
+        The step to egu conversion factor
+    acceleration : SignalRW[int]
+        The acceleration of the motor
+    _stop : SignalX
+        The signal to stop the motor
+    _state : SignalRW[DevState]
+    
+    Methods
+    -------
+    set(value: float, timeout: CalculatableTimeout = CalculateTimeout, watchers: Optional[List[Callable]] = None) -> AsyncStatus
+        Move the motor to a new position
+    stop(success: bool = False) -> AsyncStatus
+        Stop the motor
+    
+    
+    
+    """
+    position: SignalRW[float]
+    baserate: SignalRW[int]
+    slewrate: SignalRW[int]
+    conversion: SignalRW[float]
+    acceleration: SignalRW[int]
+    _stop: SignalX
+    _state: SignalRW[DevState]
+    
     # --------------------------------------------------------------------
     def __init__(self, trl: str, name: str = "", sources: dict = None) -> None:
         if sources is None:
@@ -42,10 +98,10 @@ class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
         self.src_dict["acceleration"] = sources.get("acceleration", "/Acceleration")
         self.src_dict["stop"] = sources.get("stop", "/StopMove")
         self.src_dict["state"] = sources.get("state", "/State")
-
-        for key in self.src_dict:
-            if not self.src_dict[key].startswith("/"):
-                self.src_dict[key] = "/" + self.src_dict[key]
+        
+        for key, value in self.src_dict.items():
+            if not value.startswith("/"):
+                self.src_dict[key] = "/" + value
 
         with self.add_children_as_readables(HintedSignal):
             self.position = tango_signal_rw(
@@ -70,11 +126,83 @@ class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
 
         TangoReadableDevice.__init__(self, trl, name)
         self._set_success = True
+    
+    # --------------------------------------------------------------------
+    
+    # async def _move(self, value: float, timeout: CalculatableTimeout = CalculateTimeout,  watchers: Optional[List[Callable]] = None) -> None:
+    #     if watchers is None:
+    #         watchers = []
+    #     self._set_success = True
+    #     (
+    #         old_position,
+    #         conversion,
+    #         velocity,
+    #         acceleration,
+    #     ) = await asyncio.gather(
+    #         self.position.get_value(),
+    #         self.conversion.get_value(),
+    #         self.slewrate.get_value(),
+    #         self.acceleration.get_value(),
+    #     )
+    #     if timeout is CalculateTimeout:
+    #         assert velocity > 0, "Motor has zero velocity"
+    #         timeout = (
+    #             (abs(value - old_position) * conversion / velocity)
+    #             + (2 * velocity / acceleration)
+    #             + DEFAULT_TIMEOUT
+    #         )
+    #     start = time.monotonic()
+    #     start_position = await self.position.get_value()
+        
+    #     def update_watchers(current_position: float) -> None:
+    #         for watcher in watchers:
+    #             watcher(
+    #                 name=self.name,
+    #                 current=current_position,
+    #                 initial=start_position,
+    #                 target=value,
+    #                 time_elapsed=time.monotonic() - start,
+    #             )
+        
+    #     if self.position._get_cache() is not None:
+    #         self.position.subscribe_value(update_watchers)
+        
+    #     try:
+    #         await self.position.set(value)
+    #         await asyncio.sleep(0.1)
+    #         counter = 0
+    #         state = await self._state.get_value()
+    #         while state == DevState.MOVING:
+    #             # Update the watchers with the current position every 0.5 seconds
+    #             if counter % 5 == 0:
+    #                 current_position = await self.position.get_value()
+    #                 update_watchers(current_position)
+    #                 counter = 0
+    #             await asyncio.sleep(0.1)
+    #             state = await self._state.get_value()
+    #             counter += 1
+    #     finally:
+    #         if self.position._get_cache() is not None:
+    #             self.position.clear_sub(update_watchers)
+    #     if not self._set_success:
+    #         raise RuntimeError("Motor was stopped")
+    
+    # # --------------------------------------------------------------------
 
-#    @WatchableAsyncStatus.wrap
-    async def set(
+    # def set(self, value: float, timeout: CalculatableTimeout = CalculateTimeout, watchers: Optional[List[Callable]] = None) -> AsyncStatus:
+    #     return AsyncStatus(self._move(value, timeout, watchers))
+    
+    def set(
         self,
-        new_position: float,
+        value: float,
+        timeout: CalculatableTimeout = CalculateTimeout,
+    ):
+        return WatchableAsyncStatus(self._set(value, timeout))
+    
+    #@WatchableAsyncStatus.wrap
+    async def _set(
+        self,
+        value: float,
         timeout: CalculatableTimeout = CalculateTimeout,
     ):
         self._set_success = True
@@ -92,28 +220,28 @@ class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
         if timeout is CalculateTimeout:
             assert velocity > 0, "Motor has zero velocity"
             timeout = (
-                (abs(new_position - old_position) * conversion / velocity)
+                (abs(value - old_position) * conversion / velocity)
                 + (2 * velocity / acceleration)
                 + DEFAULT_TIMEOUT
             )
 
-        await self.position.set(new_position, wait=True, timeout=timeout)
+        await self.position.set(value, wait=False, timeout=timeout)
 
-        move_status = AsyncStatus(self._wait(signal=self.position))
-        return move_status
-        #try:
-        #    async for current_position in observe_value(
-        #        self.position, done_status=move_status
-        #    ):
-        #        yield WatcherUpdate(
-        #            current=current_position,
-        #            initial=old_position,
-        #            target=new_position,
-        #            name=self.name,
-        #        )
-        #except RuntimeError as exc:
-        #    print(f"RuntimeError: {exc}")
-        #    raise
+        move_status = AsyncStatus(self._wait())
+        
+        try:
+           async for current_position in observe_value(
+               self.position, done_status=move_status
+           ):
+               yield WatcherUpdate(
+                   current=current_position,
+                   initial=old_position,
+                   target=value,
+                   name=self.name,
+               )
+        except RuntimeError as exc:
+           print(f"RuntimeError: {exc}")
+           raise
         if not self._set_success:
             raise RuntimeError("Motor was stopped")
 
@@ -123,8 +251,7 @@ class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
         return self._stop.trigger()
 
     # --------------------------------------------------------------------
-    async def _wait(self, event: Optional[Event] = None,
-                    signal: Optional[SignalR] = None) -> None:
+    async def _wait(self, event: Optional[Event] = None) -> None:
 
         await asyncio.sleep(0.5)
         state = await self._state.get_value()
@@ -133,11 +260,40 @@ class OmsVME58Motor(TangoReadableDevice, Movable, Stoppable):
                 await asyncio.sleep(0.1)
                 state = await self._state.get_value()
         except Exception as e:
-            raise RuntimeError(f"Error waiting for motor to stop: {e}")
+            raise RuntimeError(f"Error waiting for motor to stop: {e}") from e
         finally:
             if event:
                 event.set()
             if state != DevState.ON:
                 raise RuntimeError(f"Motor did not stop correctly. State {state}")
-
-
+            
+    # --------------------------------------------------------------------
+    def prepare(self, value: OmsVME58MotorConfig) -> AsyncStatus:
+        return AsyncStatus(self._prepare(value))
+    
+    # --------------------------------------------------------------------
+    async def _prepare(self, value: OmsVME58MotorConfig) -> None:
+        config = value.__dataclass_fields__
+        for key, v in config.items():
+            if v is not None:
+                if hasattr(self, key):
+                    await getattr(self, key).set(v)
+    
+    # --------------------------------------------------------------------
+    async def get_dataclass(self) -> OmsVME58MotorConfig:
+        """
+        Get the current configuration of the motor as a dataclass with attributes:
+        - baserate: int
+        - slewrate: int
+        - conversion: float
+        - acceleration: int
+        
+        Returns
+        -------
+        OmsVME58MotorConfig
+            The current configuration of the motor
+        """
+        config = OmsVME58MotorConfig()
+        for key in config.__dataclass_fields__.keys():
+            setattr(config, key, await getattr(self, key).get_value())
+        return config
