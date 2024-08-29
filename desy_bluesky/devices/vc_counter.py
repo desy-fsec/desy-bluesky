6e6565d3073fd88ab0a3ba59e3720548be553b29
Devin Burke <devin.burke@desy.de>
2024-08-29 11:57:55 +0200

    Updating VC devices

--- desy_bluesky/devices/vc_counter.py
+++ desy_bluesky/devices/vc_counter.py
@@ -1,58 +1,34 @@
 from __future__ import annotations
 
-from typing import Dict
-
-from bluesky.protocols import Reading, Preparable
+from typing import Optional, Union
 
 from ophyd_async.core import (
-    AsyncStatus,
     HintedSignal,
     soft_signal_rw,
+    SignalR,
+    SignalX
 )
 
-from ophyd_async.tango import (
-    TangoReadableDevice,
-    tango_signal_rw,
-    tango_signal_x,
-)
+from .fsec_readable_device import FSECReadableDevice
+from tango import DeviceProxy as SyncDeviceProxy
+from tango.asyncio import DeviceProxy as AsyncDeviceProxy
 
 
-class VcCounter(TangoReadableDevice, Preparable):
-    # --------------------------------------------------------------------
-    def __init__(self, trl: str, name="", sources: dict = None) -> None:
-        if sources is None:
-            sources = {}
-        self.trl = trl
-        self.src_dict["counts"] = sources.get("counts", "/Counts")
-        self.src_dict["reset"] = sources.get("reset", "/Reset")
+class VcCounter(FSECReadableDevice):
+    Counts: SignalR
+    Reset: SignalX
 
-        for key in self.src_dict:
-            if not self.src_dict[key].startswith("/"):
-                self.src_dict[key] = "/" + self.src_dict[key]
+    def __init__(
+            self,
+            trl: Optional[str] = None,
+            device_proxy: Optional[Union[AsyncDeviceProxy, SyncDeviceProxy]] = None,
+            name: str = "",
+    ) -> None:
+        super().__init__(trl, device_proxy, name)
+        self.add_readables([self.Counts], HintedSignal)
 
-        with self.add_children_as_readables(HintedSignal):
-            self.counts = tango_signal_rw(
-                float, trl + self.src_dict["counts"], device_proxy=self.proxy
-            )
-        self.reset = tango_signal_x(
-            self.trl + self.src_dict["reset"], device_proxy=self.proxy
-        )
         # Not used, I added it here because SIS3820 has it
         self.offset = soft_signal_rw(float, initial_value=0.0)
 
-        TangoReadableDevice.__init__(self, trl, name)
-        self._set_success = True
-
-    # --------------------------------------------------------------------
-    async def read(self) -> Dict[str, Reading]:
-        ret = await super().read()
-        return ret
-
-    # --------------------------------------------------------------------
-    def prepare(self, **kwargs) -> AsyncStatus:
-        return AsyncStatus(self._reset())
-
-    # --------------------------------------------------------------------
-
     async def _reset(self) -> None:
-        await self.reset.trigger()
+        await self.Reset.trigger()
--- desy_bluesky/devices/vm_motor.py
+++ desy_bluesky/devices/vm_motor.py
@@ -1,118 +1,61 @@
 from __future__ import annotations
 
-import asyncio
-from asyncio import Event
-from typing import Optional
+from typing import Optional, Union
 
-from bluesky.protocols import Movable, Stoppable
+from bluesky.protocols import Stoppable
 
 from ophyd_async.core import (
     AsyncStatus,
     ConfigSignal,
     HintedSignal,
-    WatchableAsyncStatus,
 )
-from ophyd_async.core import observe_value
+
 from ophyd_async.core import (
+    SignalR,
+    SignalX,
+    SignalRW,
     DEFAULT_TIMEOUT,
-    WatcherUpdate,
 )
-from ophyd_async.tango import (
-    TangoReadableDevice,
-    tango_signal_r,
-    tango_signal_rw,
-    tango_signal_x,
-)
-from tango import DevState
+
+from tango import DeviceProxy as SyncDeviceProxy
+from tango.asyncio import DeviceProxy as AsyncDeviceProxy
+
+from .fsec_readable_device import FSECReadableDevice
 
 
-class VmMotor(TangoReadableDevice, Stoppable, Movable):
+class VmMotor(FSECReadableDevice, Stoppable):
+    Position: SignalRW
+    CwLimit: SignalR
+    CcwLimit: SignalR
+    UnitLimitMin: SignalR
+    UnitLimitMax: SignalR
+    StopMove: SignalX
+    State: SignalR
+
     # --------------------------------------------------------------------
-    def __init__(self, trl: str, name: str = "", sources: dict = None) -> None:
-        if sources is None:
-            sources = {}
-        self.trl = trl
-        self.src_dict["position"] = sources.get("position", "/Position")
-        self.src_dict["cwlimit"] = sources.get("cwlimit", "/CWLimit")
-        self.src_dict["ccwlimit"] = sources.get("ccwlimit", "/CCWLimit")
-        self.src_dict["unitlimitmin"] = sources.get("unitlimitmin", "/UnitLimitMin")
-        self.src_dict["unitlimitmax"] = sources.get("unitlimitmax", "/UnitLimitMax")
-        self.src_dict["stop"] = sources.get("stop", "/StopMove")
-        self.src_dict["state"] = sources.get("state", "/State")
-
-        for key in self.src_dict:
-            if not self.src_dict[key].startswith("/"):
-                self.src_dict[key] = "/" + self.src_dict[key]
-
-        with self.add_children_as_readables(HintedSignal):
-            self.position = tango_signal_rw(
-                float, trl + self.src_dict["position"], device_proxy=self.proxy
-            )
-        with self.add_children_as_readables(ConfigSignal):
-            self.cwlimit = tango_signal_rw(
-                int, trl + self.src_dict["cwlimit"], device_proxy=self.proxy
-            )
-            self.ccwlimit = tango_signal_rw(
-                int, trl + self.src_dict["ccwlimit"], device_proxy=self.proxy
-            )
-            self.unitlimitmin = tango_signal_rw(
-                float, trl + self.src_dict["unitlimitmin"], device_proxy=self.proxy
-            )
-            self.unitlimitmax = tango_signal_rw(
-                float, trl + self.src_dict["unitlimitmax"], device_proxy=self.proxy
-            )
-        self._stop = tango_signal_x(trl + self.src_dict["stop"], self.proxy)
-        self._state = tango_signal_r(DevState, trl + self.src_dict["state"], self.proxy)
-
-        TangoReadableDevice.__init__(self, trl, name)
+    def __init__(
+            self,
+            trl: Optional[str] = None,
+            device_proxy: Optional[Union[AsyncDeviceProxy, SyncDeviceProxy]] = None,
+            name: str = "",
+    ) -> None:
+        super().__init__(trl, device_proxy, name)
+        self.add_readables([self.Position], HintedSignal)
+        self.add_readables([self.CwLimit,
+                            self.CcwLimit,
+                            self.UnitLimitMin,
+                            self.UnitLimitMax], ConfigSignal)
         self._set_success = True
 
     # --------------------------------------------------------------------
 
-    @WatchableAsyncStatus.wrap
+    @AsyncStatus.wrap
     async def set(self, new_position: float, timeout: float = DEFAULT_TIMEOUT):
+        await self.Position.set(new_position, wait=True, timeout=timeout)
         self._set_success = True
-        old_position = await self.position.get_value()
-
-        await self.position.set(new_position, wait=True, timeout=timeout)
-
-        move_status = AsyncStatus(self._wait())
-
-        try:
-            async for current_position in observe_value(
-                self.position, done_status=move_status
-            ):
-                yield WatcherUpdate(
-                    current=current_position,
-                    initial=old_position,
-                    target=new_position,
-                    name=self.name,
-                )
-        except RuntimeError as exc:
-            print(f"RuntimeError: {exc}")
-            raise
-        if not self._set_success:
-            raise RuntimeError("Motor was stopped")
 
     # --------------------------------------------------------------------
-    async def stop(self, success: bool = False) -> None:
+    @AsyncStatus.wrap
+    async def stop(self, success=True) -> None:
         self._set_success = success
-        # Put with completion will never complete as we are waiting for completion on
-        # the move above, so need to pass wait=False
-        await self._stop.trigger()
-
-    # --------------------------------------------------------------------
-    async def _wait(self, event: Optional[Event] = None) -> None:
-        await asyncio.sleep(0.5)
-        state = await self._state.get_value()
-        try:
-            while state == DevState.MOVING:
-                await asyncio.sleep(0.1)
-                state = await self._state.get_value()
-        except Exception as e:
-            raise RuntimeError(f"Error waiting for motor to stop: {e}")
-        finally:
-            if event:
-                event.set()
-            if state != DevState.ON:
-                raise RuntimeError(f"Motor did not stop correctly. State {state}")
+        await self.StopMove.trigger(wait=True)
