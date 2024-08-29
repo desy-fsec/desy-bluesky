60ed15cfba3b2be92d0fb5b024bba41975deaca7
Devin Burke <devin.burke@desy.de>
2024-08-29 12:05:15 +0200

    Updating undulator

--- desy_bluesky/devices/undulator.py
+++ desy_bluesky/devices/undulator.py
@@ -1,120 +1,62 @@
 from __future__ import annotations
 
-from typing import Dict
-from typing import Optional
+from typing import Optional, Union
 
-from bluesky.protocols import Movable, Stoppable, Reading, Preparable, Status
-
-import asyncio
-from asyncio import Event
+from bluesky.protocols import Movable, Stoppable
 
 from ophyd_async.core import (
+    DEFAULT_TIMEOUT,
     soft_signal_rw,
     AsyncStatus,
     HintedSignal,
     ConfigSignal,
+    SignalRW,
+    WatchableAsyncStatus,
+    SignalR,
+    SignalX
 )
 
-from ophyd_async.core import (
-    DEFAULT_TIMEOUT,
-)
-from ophyd_async.tango import (
-    TangoReadableDevice,
-    tango_signal_r,
-    tango_signal_rw,
-    tango_signal_x,
-)
-from tango import DevState
+from tango import DeviceProxy as SyncDeviceProxy
+from tango.asyncio import DeviceProxy as AsyncDeviceProxy
+
+from .fsec_readable_device import FSECReadableDevice
 
 
-class Undulator(TangoReadableDevice, Stoppable, Preparable, Movable):
-    trl: str
-    name: str
-    src_dict: dict
-    offset: float
-    position: tango_signal_rw
-    _state: tango_signal_r
-    _stop: tango_signal_x
+class Undulator(FSECReadableDevice, Movable, Stoppable):
+    Position: SignalRW
+    State: SignalR
+    StopMove: SignalX
 
     def __init__(
-        self, trl: str, name: str = "", sources: dict = None, offset: float = 0
+            self,
+            trl: Optional[str] = None,
+            device_proxy: Optional[Union[AsyncDeviceProxy, SyncDeviceProxy]] = None,
+            name: str = "",
+            offset: float = 0.0,
     ) -> None:
-        if sources is None:
-            sources = {}
-        self.trl = trl
-        self.src_dict = sources
-        self.src_dict["position"] = sources.get("position", "/Position")
-        self.src_dict["state"] = sources.get("state", "/State")
-        self.src_dict["stop"] = sources.get("stop", "/StopMove")
-        
-
-        for key in self.src_dict:
-            if not self.src_dict[key].startswith("/"):
-                self.src_dict[key] = "/" + self.src_dict[key]
-
-        with self.add_children_as_readables(HintedSignal):
-            self.position = tango_signal_rw(
-                float, trl + self.src_dict["position"], device_proxy=self.proxy
-            )
-        self._state = tango_signal_r(DevState, trl + self.src_dict["state"], self.proxy)
-        self._stop = tango_signal_x(
-            trl + self.src_dict["stop"], device_proxy=self.proxy
-        )
-        
+        super().__init__(trl, device_proxy, name)
+        self.add_readables([self.Position], HintedSignal)
         with self.add_children_as_readables(ConfigSignal):
             self.offset = soft_signal_rw(float, initial_value=offset)
-
-        TangoReadableDevice.__init__(self, trl, name)
         self._set_success = True
 
     # --------------------------------------------------------------------
 
-#    @WatchableAsyncStatus.wrap
+    @WatchableAsyncStatus.wrap
     async def set(
         self,
         new_position: float,
         timeout: float = DEFAULT_TIMEOUT,
     ):
         self._set_success = True
-        old_position = await self.position.get_value()
 
-        await self.position.set(new_position, wait=True, timeout=timeout)
+        await self.Position.set(new_position, timeout=timeout)
 
-        move_status = AsyncStatus(self._wait())
+        move_status = self.wait_for_idle()
         return move_status
 
     # --------------------------------------------------------------------
 
-    def stop(self, success: bool = False) -> AsyncStatus:
+    def stop(self, success: bool = True) -> AsyncStatus:
         self._set_success = success
-        return self._stop.trigger()
-
-    # --------------------------------------------------------------------
-
-    async def read(self) -> Dict[str, Reading]:
-        ret = await super().read()
-        return ret
-
-    # --------------------------------------------------------------------
-
-    def prepare(self, value: float) -> Status:
-        self.offset.put(value)
-        # Return a dummy status
-        return Status()
-
-    # --------------------------------------------------------------------
-
-    async def _wait(self, event: Optional[Event] = None) -> None:
-        await asyncio.sleep(0.5)
-        state = await self._state.get_value()
-        try:
-            while state == DevState.MOVING:
-                await asyncio.sleep(0.1)
-                state = await self._state.get_value()
-        except Exception as e:
-            raise RuntimeError(f"Error waiting for motor to stop: {e}")
-        finally:
-            if event:
-                event.set()
-            if state != DevState.ON:
-                raise RuntimeError(f"Motor did not stop correctly. State {state}")
+        return self.StopMove.trigger()
