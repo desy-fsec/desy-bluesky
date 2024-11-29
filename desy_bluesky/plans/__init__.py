7da9381e92a1d0991fd606bc792bafbf6b4d7e3c
Devin Burke <devin.burke@desy.de>
2024-11-29 13:43:09 +0100

    Created stubs for ramp and dwell

--- desy_bluesky/plans/__init__.py
+++ desy_bluesky/plans/__init__.py
@@ -1,4 +1,8 @@
 from .preprocessors import InjectMD
 from .continuous_scan import continuous_scan
+from .stubs import ramp, dwell
 
-_all__ = ['InjectMD', 'continuous_scan']
+_all__ = ['InjectMD',
+          'continuous_scan',
+          'ramp',
+          'dwell']
